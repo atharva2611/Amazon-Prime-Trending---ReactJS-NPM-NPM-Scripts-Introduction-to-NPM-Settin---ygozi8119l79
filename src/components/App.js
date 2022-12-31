9f35266494dfb01b555991b0b9b7ec8cda953d89
sandeepitgcet <sandeepitgcet@gmail.com>
2022-12-31 16:25:25 +0530

    updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import slides from "../data";
-const App = () => {
+
+const App = (props) => {
+  const [slide,setSlide] = useState(props.slides[0]);
   return (
     <div>
-      {slides.map(slide=>{
-        return (
-          <div>
-            <h1>{slide.text}</h1>
-            <p>{slide.title}</p>
-          </div>
-        );
-      })}
+        <div>
+            <h1>{slide.title}</h1>
+            <p>{slide.text}</p>
+        </div>
+        <div>
+          <button id="restart" data-testid="button-restart">Restart</button>
+          <button id="prev" data-testid="button-prev">Prev</button>
+          <button id="next" data-testid="button-next">Next</button>
+        </div>
     </div>
   )
 }
