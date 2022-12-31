1bc37458c414086915047fb36c7c8423e1a6091c
sandeepitgcet <sandeepitgcet@gmail.com>
2022-12-31 16:16:31 +0530

    updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import slides from "../data";
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      {slides.map(slide=>{
+        return (
+          <div>
+            <h1>slide.text</h1>
+            <p>slide.title</p>
+          </div>
+        );
+      })}
     </div>
   )
 }
