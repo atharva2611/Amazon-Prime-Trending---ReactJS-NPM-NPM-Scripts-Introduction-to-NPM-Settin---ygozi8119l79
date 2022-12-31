df7d8fa25c103e9c980a0dd155569f1c1d245ad1
sandeepitgcet <sandeepitgcet@gmail.com>
2022-12-31 17:02:22 +0530

    updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,22 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = (props) => {
-  const [slide,setSlide] = useState(props.slides[0]);
   const [index,setIndex] = useState(0);
+
   const nextHandle = () =>{
     if(index<props.slides.length){
         setIndex(index+1);
-        setSlide(props.slides[index]);
         document.getElementById("prev").disabled=false;
         document.getElementById("restart").disabled=false;
     }
 
-    if(index==props.length){
+    if(index==props.length-1){
       document.getElementById("next").disabled=true;
     }
   }
   const prevHandle = () =>{
     if(index>0){
         setIndex(index-1);
-        setSlide(props.slides[index]);
         document.getElementById("next").disabled=false;
     }
     if(index==0){
@@ -30,17 +28,14 @@ const App = (props) => {
   const restartHandle = () =>{
     if(index!=0){
         setIndex(0);
-        setSlide(props.slides[index]);
-        if(index==props.length){
-          document.getElementById("restart").disabled=true;
-        }
+        document.getElementById("restart").disabled=true;
     }
   }
   return (
     <div>
         <div>
-            <h1 data-testid="title">{slide.title}</h1>
-            <p data-testid="text">{slide.text}</p>
+            <h1 data-testid="title">{props.slide[index].title}</h1>
+            <p data-testid="text">{props.slide[index].text}</p>
         </div>
         <div>
           <button id="restart" data-testid="button-restart" disabled onClick={restartHandle}>Restart</button>
